ce1649f34bf5cc43e7a5360e7ab4eb11b774fddc
Mitsutaka Takeda <mitsutaka.takeda@gmail.com>
2016-02-25 01:49:00 +0900

    Remove unused structs

--- main.cpp
+++ main.cpp
@@ -14,35 +14,6 @@
 #include "stlab/future.hpp"
 
 namespace  {
-    struct file_reader {
-        file_reader() = default;
-        file_reader(boost::interprocess::file_mapping&& f_,
-                    boost::uintmax_t s_)
-            : f(std::move(f_)),
-              m(f, f.get_mode(), 0, s_)
-            {}
-        boost::interprocess::file_mapping f;
-        boost::interprocess::mapped_region m;
-    };
-
-    struct result {
-    private:
-        file_reader fr;
-        std::string s;
-    public:
-        using const_iterator = std::vector<std::match_results<const char*> >::const_iterator;
-        result() = default;
-        result(file_reader&& fr_,
-               std::string s_)
-            : fr(std::move(fr_)),
-              s(std::move(s_)) {
-        }
-
-        const std::string& str() const noexcept {
-            return s;
-        }
-    };
-
     template< class CharT, class Traits >
     std::basic_ostream<CharT, Traits>& newline(std::basic_ostream<CharT, Traits>& os) noexcept {
         // return os.put(os.widen('\n'));
@@ -73,11 +44,11 @@ int main(int argc, char * argv[]) try {
                       tasks.push_back(stlab::async(
                           scheduler,
                           [p = std::move(p), &pattern](){
-                              file_reader 
-                                  fr{boost::interprocess::file_mapping(p.string().c_str(), boost::interprocess::read_only),
-                                      boost::filesystem::file_size(p)};
-                              auto const begin = static_cast<char const*>(fr.m.get_address()),
-                                  end = static_cast<char const*>(fr.m.get_address()) + fr.m.get_size();
+                              boost::interprocess::file_mapping fm(p.string().c_str(), boost::interprocess::read_only);
+                              boost::interprocess::mapped_region m(fm, fm.get_mode(), 0, boost::filesystem::file_size(p));
+
+                              auto const begin = static_cast<char const*>(m.get_address()),
+                                  end = static_cast<char const*>(m.get_address()) + m.get_size();
                               if(!utf8::is_valid(begin, end)){
                                   return ""s;
                               }
