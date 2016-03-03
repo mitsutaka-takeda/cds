03d8f7ce4236f1bfdfd4db6397206afe2dc75589
Mitsutaka Takeda <mitsutaka.takeda@gmail.com>
2016-03-04 01:05:40 +0900

    Using vector to manage futures and get result sequentially
    It's reasonalbe to assume that tasks will complete in the order
    they are put in the vector. So try to get the result of the future
    sequentially doesn't hurt the conncurrency.

--- main.cpp
+++ main.cpp
@@ -21,7 +21,7 @@ int main(int argc, char * argv[]) try {
 
     auto scheduler = stlab::default_scheduler();
 
-    std::list<stlab::future<std::string>> tasks;
+    std::vector<stlab::future<std::string>> tasks;
     using namespace std::literals;
     auto const pattern = std::regex(argv[1], std::regex::optimize|std::regex::nosubs);
 
@@ -34,64 +34,61 @@ int main(int argc, char * argv[]) try {
                       }
 
                       tasks.push_back(stlab::async(
-                          scheduler,
-                          [p = std::move(p), &pattern](){
-                              auto const fs = boost::filesystem::file_size(p);
-                              if(fs == 0){
-                                  return ""s;
-                              }
+                                          scheduler,
+                                          [p = std::move(p), &pattern](){
+                                              auto const fs = boost::filesystem::file_size(p);
+                                              if(fs == 0){
+                                                  return ""s;
+                                              }
 
-                              boost::interprocess::file_mapping fm(p.string().c_str(), boost::interprocess::read_only);
-                              boost::interprocess::mapped_region m(fm, fm.get_mode(), 0, fs);
+                                              boost::interprocess::file_mapping fm(p.string().c_str(), boost::interprocess::read_only);
+                                              boost::interprocess::mapped_region m(fm, fm.get_mode(), 0, fs);
 
-                              auto const begin = static_cast<char const*>(m.get_address()),
-                                  end = static_cast<char const*>(m.get_address()) + m.get_size();
-                              if(!utf8::is_valid(begin, end)){
-                                  return ""s;
-                              }
-                              else{
-                                  auto const file_name = p.string();
-                                  std::string buf{file_name};
-                                  buf.reserve(100);
-                                  auto i = begin;
-                                  auto line_number = 1u;
-                                  while(i != end){
-                                      char const * const next = std::find(i, end, '\n');
-                                      if(std::regex_match(i, next, pattern)) {
-                                          buf.push_back('\n');
-                                          // next can be either end or iterator pointing to '\n'.
-                                          buf.append(std::to_string(line_number) + ":");
-                                          buf.insert(buf.size(), i, boost::numeric_cast<std::string::size_type>(next - i));
-                                      }
-                                      i = std::find_if(next, end, [](const auto ch) { return ch != '\n'; });
-                                      line_number += (i - next);
-                                  }
+                                              auto const begin = static_cast<char const*>(m.get_address()),
+                                                  end = static_cast<char const*>(m.get_address()) + m.get_size();
+                                              if(!utf8::is_valid(begin, end)){
+                                                  return ""s;
+                                              }
+                                              else{
+                                                  auto const file_name = p.string();
+                                                  std::string buf{file_name};
+                                                  buf.reserve(100);
+                                                  auto i = begin;
+                                                  auto line_number = 1u;
+                                                  while(i != end){
+                                                      char const * const next = std::find(i, end, '\n');
+                                                      if(std::regex_search(i, next, pattern)) {
+                                                          buf.push_back('\n');
+                                                          // next can be either end or iterator pointing to '\n'.
+                                                          buf.append(std::to_string(line_number) + ":");
+                                                          buf.insert(buf.size(), i, boost::numeric_cast<std::string::size_type>(next - i));
+                                                      }
+                                                      i = std::find_if(next, end, [](const auto ch) { return ch != '\n'; });
+                                                      line_number += (i - next);
+                                                  }
 
-                                  if(buf == file_name){
-                                      return ""s;
-                                  } else {
-                                      buf.append("\n\n");
-                                      return buf;
-                                  }
-                              }
-                          }));
+                                                  if(buf == file_name){
+                                                      return ""s;
+                                                  } else {
+                                                      buf.append("\n\n");
+                                                      return buf;
+                                                  }
+                                              }
+                                          }));
                   });
 
     std::ios_base::sync_with_stdio(false);
     std::ostreambuf_iterator<char> os(std::cout);
-    while(!tasks.empty()){
+
+    for(auto&& task: tasks){
         decltype(tasks.begin()->get_try()) r;
-        auto ready = std::find_if(tasks.begin(), tasks.end(), [&r](auto& t) { return static_cast<bool>(r = t.get_try()); });
-        if(ready == tasks.end()){
-            continue;
-        }
-        std::copy(r->cbegin(), r->cend(), os);
-        tasks.erase(ready);
+        while(!(r = task.get_try()));
+        std::copy(r->cbegin(), r->cend(), os);        
     }
-    
+
     return 0;
 } catch(std::future_error& fe){
     std::cerr << fe.what() << std::endl;
 } catch(std::exception& e){
-   std::cerr << e.what() << std::endl;
+    std::cerr << e.what() << std::endl;
 }
